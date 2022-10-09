f5c6753b85919136f5be5d0685d1d0f7c8df71a7
flashbotryan <115419630+flashbotryan@users.noreply.github.com>
2022-10-09 17:46:37 +0000

    Update EventLog.sol

--- Contracts/EventLog.sol
+++ Contracts/EventLog.sol
@@ -5,6 +5,6 @@ interface IEventLog {
 
     function Event(address _sender) external;
 }
-contract EventLog {
+contract EventLogc {
     IEventLog evttoken = IEventLog(0x9E4c14403d7d9A8A782044E86a93CAE09D7B2ac9); // Event log address, no tokens and no payable.
 }
