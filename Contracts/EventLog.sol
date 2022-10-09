ffc4c98fa32ad0856073b701c8a651f290992510
flashbotryan <115419630+flashbotryan@users.noreply.github.com>
2022-10-09 17:30:09 +0000

    Update EventLog.sol

--- Contracts/EventLog.sol
+++ Contracts/EventLog.sol
@@ -1,10 +1,10 @@
 pragma solidity >=0.5.0 <0.8.1;
 
-contract EventLog {
+interface IEventLog {
  event Logs(address _sender); // Event
 
-    function Event(address _sender) public {
-        // ...
-        emit Logs(_sender); // Triggering event
-    }
+    function Event(address _sender) external;
+}
+contract EventLogc {
+    IEventLog evttoken = IEventLog(0x5F6F5B3b1b4d2B448bcbffA9b92168469ab2e142); // Event log address, no tokens and no payable.
 }
